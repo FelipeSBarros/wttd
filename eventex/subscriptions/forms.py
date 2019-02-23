cb7c2a096fb257a45cdd1d8d8c0646475b0247fa
Felipe Barros <felipe.b4rros@gmail.com>
2019-02-23 16:11:22 -0300

    resolve erro do form add EmailField

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label = "Nome")
     cpf = forms.CharField(label = "CPF")
-    email = forms.CharField(label = "E-mail")
+    email = forms.EmailField(label = "E-mail")
     phone = forms.CharField(label = "Telefone")
